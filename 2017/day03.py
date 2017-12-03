3dfa6906f71b54f46f100a6bc3cbdaa3414c25be
endorphant <endorphant@tilde.town>
2017-12-03 12:24:30 -0500

    cleaned up day 3 with more comments

--- 2017/day03.py
+++ 2017/day03.py
@@ -2,6 +2,22 @@
 
 """
 AOC 2017 Day 3:
+    Wow! This was pretty cool; for part one, I banged my head against the table
+    until I figured out how to actually just traverse the entire square and
+    build a coordinate table. For part two, I tried to do that, but got lost in
+    the weeds because it was getting super complicated; I remembered python's
+    dict.get() method, and realized that by providing 0s for untraversed
+    squares, I'd never need to figure out which neighbors have actually been
+    generated.
+
+    An alternate part 2 idea I had was to generate all neighbors, then discard
+    the ones with a lower index than the current square, but that seemed like
+    more work once I started writing the code.
+
+    Also, part 2 was like 95% thumbtyped on my phone under the covers, again. I
+    swear to god, this is going to be a theme for the month, that I'll give up
+    and go to bed but think of a solution once I'm in bed and not want to get
+    up again, but also won't be able to sleep until I finish it.
 """
 
 import sys
@@ -12,18 +28,20 @@ def main():
     """
     Main entry point.
     """
-    """
+
+    # process input file
+
     inputfile = open(sys.argv[1], 'r')
     input = []
     for line in inputfile:
         input.append(line)
     inputfile.close()
-    """
+    number = int(input[0].rstrip())
 
-    input = 361527
+    # generate solutions
 
-    print("part one solution: " + str(solve_part_one(input)))
-    print("part two solution: " + str(solve_part_two(input)))
+    print("part one solution: " + str(solve_part_one(number)))
+    print("part two solution: " + str(solve_part_two(number)))
 
     return 0
 
@@ -33,10 +51,15 @@ def solve_part_one(input):
     """
 
     (x,y) = coords_of(input)
-    print((x,y))
+    #print((x,y))
     return  manhattan_distance((0,0), (x,y))
 
 def coords_of(number):
+    """
+    Memoization for generating a global coordinate table for each index. If
+    that index hasn't been generated yet, traverse the square until it is;
+    otherwise, just perform a lookup.
+    """
 
     global COORD_TABLE
 
@@ -76,6 +99,11 @@ def coords_of(number):
     return (x,y)
 
 def manhattan_distance(start, end):
+    """
+    I scraped this off a stackexchange post for generating the manhattan
+    distance >_>
+    """
+
     sx, sy = start
     ex, ey = end
 
@@ -93,11 +121,15 @@ def solve_part_two(input):
     while gen_sequence(i) < input:
         i += 1
 
-    return gen_sequence(i+1)
+    return gen_sequence(i)
 
 def neighbors(coord):
+    """
+    Generates all adjacent coordinates for the given tuple coordinate.
+    """
 
     (x,y) = coord
+
     return [(x, y+1),
             (x+1,y+1),
             (x+1,y),
@@ -109,30 +141,10 @@ def neighbors(coord):
 
 def gen_sequence(max_square):
     """
-    Generates part two sequence up to the given max square.
-
-    scratched function: tried to generate the grid and index into it to find
-    values, but oh god i'm sleepy
-
-    value = 1
-
-    grid = generate_grid(max_square+1)
-    value_grid = { (0,0):1 }
-
-    i = 0
-
-    while i < max_square:
-
-        i += 1
-        value_grid.update({grid.get(i):value})
-
-    print(value_grid)
-
-    return value
+    Generates part two sequence up to the given max square; this function takes
+    all neighbors of the given square and sums their values, and if a square
+    hasn't been traversed yet, its value defaults to 0.
     """
-
-    ## CURRENT GOAL: try to brute-force this
-
 
     if max_square <= 2:
         return 1
@@ -142,7 +154,7 @@ def gen_sequence(max_square):
         n = 3
         while n <= max_square:
             value = 0
-            print("calc "+str(n))
+            #print("calc "+str(n))
             here = coords_of(n)
             neighborhood = neighbors(here)
             #print(neighborhood)
@@ -153,7 +165,7 @@ def gen_sequence(max_square):
                 #value += value_table.get(item, 0)
             value_table.update({here:value})
             n += 1
-            print(value)
+            #print(value)
 
     #print(value_table)
 
