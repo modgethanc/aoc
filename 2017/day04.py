dead53e4ad4f5ae5e60a5aa7e53e5dd03b8325d1
endorphant <endorphant@tilde.town>
2017-12-04 12:20:25 -0500

    day 4 complete

---
+++ 2017/day04.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+"""
+AOC 2017 Day 4:
+"""
+
+import sys
+
+def main():
+    """
+    Main entry point.
+    """
+
+    inputfile = open(sys.argv[1], 'r')
+    input = []
+    for line in inputfile:
+        input.append(line)
+    inputfile.close()
+
+    print("part one solution: " + str(solve_part_one(input)))
+    print("part two solution: " + str(solve_part_two(input)))
+
+    return 0
+
+def solve_part_one(input):
+    """
+    Solver for part one.
+    """
+
+    solution = 0
+
+    for test in input:
+        test = test.rstrip()
+        #print("testing: "+test)
+        if validate(test):
+            solution += 1
+
+    return solution
+
+def validate(passphrase):
+    """
+    Checks if a single passphrase is valid; returns true if so.
+    """
+
+    wordlist = passphrase.split()
+    checklist = set(wordlist)
+
+    if len(wordlist) == len(checklist):
+        return True
+    else:
+        #print("INVALID")
+        return False
+
+def solve_part_two(input):
+    """
+    Solver for part two.
+    """
+
+    solution = 0
+
+    for test in input:
+        test = test.rstrip()
+        #print("testing: "+test)
+        if validate(test):
+            if validate_anagrams(test):
+                solution += 1
+
+    return solution
+
+def validate_anagrams(passphrase):
+    """
+    Checks a single passphrase for anagrams; returns true if it has none, false
+    otherwise.
+    """
+
+    wordlist = passphrase.split()
+    #print(wordlist)
+
+    for word in wordlist:
+        #print("comparing: "+word)
+        for check in wordlist:
+            #print("     to: "+check)
+            if word == check:
+                #print("...skip self compare...")
+                pass
+            elif is_anagram(word, check):
+                return False
+
+    return True
+
+def is_anagram(a, b):
+    """
+    Checks if two words are anagrams of each other; returns true if so.
+    """
+
+    if len(a) != len(b):
+        #print("not anagram: not the same length")
+        return False
+    else:
+        a_list = sorted(list(a))
+        b_list = sorted(list(b))
+
+        #print(a_list)
+        #print(b_list)
+
+        if a_list == b_list:
+            #print("anagram found")
+            return True
+        else:
+            #print("no anagram found")
+            return False
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ 2017/test_day04.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+"""
+Testing module for day 4.
+"""
+
+import unittest
+from day04 import validate
+from day04 import validate_anagrams
+from day04 import is_anagram
+
+class PartOneTestCase(unittest.TestCase):
+    """
+    Tests for solve_part_one
+    """
+
+    def test_example_one(self):
+        self.assertTrue(validate("aa bb cc dd ee"))
+
+    def test_example_two(self):
+        self.assertFalse(validate("aa bb cc dd aa"))
+
+    def test_example_three(self):
+        self.assertTrue(validate("aa bb cc dd aaa"))
+
+class PartTwoTestCase(unittest.TestCase):
+    """
+    Tests for solve_part_two
+
+    def test_anagram_one(self):
+        print("test one")
+        self.assertTrue(is_anagram("aba", "baa"))
+
+    def test_anagram_two(self):
+        print("test two")
+        self.assertFalse(is_anagram("aba", "baaa"))
+
+    def test_anagram_three(self):
+        print("test three")
+        self.assertFalse(is_anagram("aba", "aca"))
+    """
+
+    def test_example_one(self):
+        print("validate one")
+        self.assertTrue(validate_anagrams("abcde fghij"))
+
+    def test_example_two(self):
+        print("validate two")
+        self.assertFalse(validate_anagrams("abcde xyz ecdab"))
+
+    def test_example_three(self):
+        print("validate three")
+        self.assertTrue(validate_anagrams("a ab abc abd abf abj"))
+
+    def test_example_four(self):
+        print("validate four")
+        self.assertTrue(validate_anagrams("iiii oiii ooii oooi oooo"))
+
+    def test_example_five(self):
+        print("validate five")
+        self.assertFalse(validate_anagrams("oiii ioii iioi iiio"))
+    """
+    """
+
+if __name__ == '__main__':
+    unittest.main()
