3e89331b275e738d40c94d902d4c471f0ec7f731
endorphant <endorphant@tilde.town>
2017-12-06 12:25:49 -0500

    adding shortcuts

---
+++ 2017/shortcuts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Shortcuts for AoC.
+"""
+
+def intify(str_list):
+    """
+    Takes a list of stings that are assumed to refer to integers and returns a list of integers.
+    """
+
+    return [int(item) for item in str_list]
