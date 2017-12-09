afa913b62b63a4f6b21cad4530922491d90dc590
endorphant <endorphant@tilde.town>
2017-12-08 23:17:26 -0500

    day 7 part 2 done

--- 2017/day07-test.py
+++ 2017/day07-test.py
@@ -43,17 +43,44 @@ class PartTwoTestCase(unittest.TestCase):
     """
     Tests for solve_part_two
     """
-    '''
 
     inputfile = open("day07-sample.txt", 'r')
-    inputs = []
+    sample = []
     for line in inputfile:
-        inputs.append(line)
+        sample.append(line.rstrip())
     inputfile.close()
 
-    def test_example_one(self):
-        self.assertEqual(solve_part_one(self.inputs), 18)
+    inputfile = open("day07-input.txt", 'r')
+    inputs = []
+    for line in inputfile:
+        inputs.append(line.rstrip())
+    inputfile.close()
 
+    sampletree = make_tree(sample)
+    #print(sampletree)
+    #print(tower_weights("tknk", sampletree))
+
+    def test_example_one(self):
+        self.assertEqual(solve_part_two(self.sample), 60)
+
+    def test_get_weight(self):
+        self.assertEqual(get_weight("fwft (72) -> ktlj, cntj, xhth"), 72)
+
+    def test_sum_weights(self):
+        self.assertEqual(sum_weights("ugml", self.sampletree), 251)
+
+    def test_sum_weights_two(self):
+        self.assertEqual(sum_weights("padx", self.sampletree), 243)
+    
+    def test_find_culprits(self):
+        self.assertEqual(find_culprit(tower_weights("tknk", self.sampletree)), ("ugml", 243))
+    
+    '''
+    def test_balance_checker(self):
+        self.assertTrue(is_balanced("padx", self.sampletree))
+    
+    def test_balance_checker_false(self):
+        self.assertFalse(is_balanced("tknk", self.sampletree))
     def test_wrong(self):
         self.assertGreater(solve_part_one(self.inputs), 344)
     '''
--- 2017/day07.py
+++ 2017/day07.py
@@ -16,8 +16,8 @@ def main():
         inputs.append(line.rstrip())
     inputfile.close()
 
-    print("part one solution: " + str(solve_part_one(inputs)))
-    #print("part two solution: " + str(solve_part_two(inputs)))
+    #print("part one solution: " + str(solve_part_one(inputs)))
+    print("part two solution: " + str(solve_part_two(inputs)))
 
     return 0
 
@@ -83,9 +83,127 @@ def solve_part_two(inputs):
 
     solution = 0
 
+    input_tree = make_tree(inputs)
+    culprits = find_unbalanced_items(input_tree)
+    unbalanced = []
+    for culprit in culprits:
+        unbalanced.append(find_culprit(tower_weights(culprit, input_tree)))
 
+    print(unbalanced)
+    unbalanced.sort(key=lambda item:item[1])
+
+    culprit = unbalanced[0]
+
+    culprit_weight = sum_weights(culprit[0], input_tree)
+    print(culprit_weight)
+    delta = culprit[1] - culprit_weight
+    print(delta)
+
+    solution = input_tree.get(culprit[0]).get("weight") + delta
 
     return solution
+
+def get_weight(line):
+    """
+    Given a single line of input, return the weight as an int of the base item.
+    """
+
+    # this split is ugly but it works
+
+    return int(line.split("(")[1].split(")")[0])
+
+def make_tree(inputs):
+    """
+    Given a full set of inputs, return a dict that represents each item in the
+    following way:
+        "item": {
+            "children":["child1", "child2", "child3"],
+            "weight": weight
+            }
+    If the item has no children, its value should be blank.
+    """
+
+    tree = {}
+
+    for line in inputs:
+        tree.update({get_base(line):{
+            "children": get_children(line),
+            "weight": get_weight(line)
+                }
+            })
+
+    return tree
+
+def sum_weights(item, tree):
+    """
+    Given a single item, return the weight of its entire tower based on the
+    given input set.
+    """
+
+    total = tree.get(item).get("weight")
+
+    for child in tree.get(item).get("children"):
+        total += sum_weights(child, tree)
+
+    return total
+
+def tower_weights(item, tree):
+    """
+    Given a single item, return a list of tuples of (item, weight) representing
+    the total weight of each of its children, sorted by weight.
+    """
+
+    weights = []
+
+    for child in tree.get(item).get("children"):
+        weights.append((child, sum_weights(child, tree)))
+
+    weights.sort(key=lambda item:item[1])
+
+    return weights
+
+def is_balanced(item, tree):
+    """
+    Given a single item, return whether or not the values of its children are
+    balanced.
+    """
+
+    weights = []
+    weight_chart = tower_weights(item, tree)
+
+    for sum in weight_chart:
+        weights.append(sum[1])
+
+    return len(set(weights)) <= 1
+
+def find_unbalanced_items(tree):
+    """
+    Given a single tree, return all unbalanced items in it.
+    """
+
+    culprits = []
+
+    for item in tree:
+        if not is_balanced(item, tree):
+            culprits.append(item)
+
+    return culprits
+
+def find_culprit(culprits):
+    """
+    Given a list of unbalanced tuples (item, weight), return a tuple of
+    (culprit, desired weight)
+    """
+
+    smallest = culprits[0][1]
+    biggest = culprits[-1][1]
+    comp = culprits[1][1]
+
+    if smallest == comp:
+        # this means the biggest is unique
+        return (culprits[-1][0], smallest)
+    else:
+        return (culprits[0][0], biggest)
 
 if __name__ == '__main__':
     sys.exit(main())
