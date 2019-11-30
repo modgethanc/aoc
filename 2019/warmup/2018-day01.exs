d2162642c8164d706cbb6aba98e89e7e5b61be54
Vincent Zeng <hvincent@modgethanc.com>
2019-11-29 20:49:06 -0500

    better function

--- 2019/warmup/2018-day01.exs
+++ 2019/warmup/2018-day01.exs
@@ -1,11 +1,15 @@
 defmodule Math do
     def sum_list([head | tail], accumulator) do
           sum_list(tail, head + accumulator)
-        end
+    end
 
     def sum_list([], accumulator) do
           accumulator
-        end
+    end
+
+    def better_sum_list(list, acc) do
+          Enum.reduce(list, acc, &+/2)
+    end
 end
 
 
@@ -14,21 +18,30 @@ end
 # test against examples
 
 test1 = Math.sum_list([+1, +1, +1],0)
+test1b = Math.better_sum_list([+1, +1, +1],0)
 test2 = Math.sum_list([+1, +1, -2],0)
+test2b = Math.better_sum_list([+1, +1, -2],0)
 test3 = Math.sum_list([-1, -2, -3],0)
+test3b = Math.better_sum_list([-1, -2, -3],0)
 
 IO.puts("test 1:")
 IO.puts(test1 == 3)
+IO.puts("test 1b:")
+IO.puts(test1b == 3)
 IO.puts("test 2:")
 IO.puts(test2 == 0)
+IO.puts("test 2b:")
+IO.puts(test2b == 0)
 IO.puts("test 3:")
 IO.puts(test3 == -6)
+IO.puts("test 3b:")
+IO.puts(test3b == -6)
 
 # using puzzle input
 
-File.read!("2019-day01-input.txt")
+File.read!("2018-day01-input.txt")
 |> String.trim()
 |> String.split()
 |> Enum.map(&String.to_integer/1)
-|> Math.sum_list(0)
+|> Math.better_sum_list(0)
 |> IO.puts()
