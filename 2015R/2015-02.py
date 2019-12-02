c6918a2aa060b0ceb38f958f53b26e31b7005959
Vincent Zeng <hvincent@modgethanc.com>
2019-12-02 17:38:33 -0500

    2015d2 complete

--- 2015R/2015-02.py
+++ 2015R/2015-02.py
@@ -7,6 +7,7 @@ for x in open("2015-02-input.txt").readlines():
     d = list(map(int, x.rstrip().split('x')))
     d.sort()
     paper += 2*(d[0]*d[1]+ d[1]*d[2] + d[0]*d[2]) + d[0]*d[1]
+    ribbon += 2*d[0] + 2*d[1] + d[0]*d[1]*d[2]
 
 print(paper)
 print(ribbon)
