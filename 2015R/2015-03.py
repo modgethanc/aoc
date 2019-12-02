ee9d60bb823e6a1ea9b8aeaf55fdc9b507fb0407
Vincent Zeng <hvincent@modgethanc.com>
2019-12-02 18:43:34 -0500

    golfing

--- 2015R/2015-03.py
+++ 2015R/2015-03.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 
+def move(start, direction):
+    if x == "^":
+        return(start[0]+1, start[1])
+    elif x == ">":
+        return(start[0], start[1]+1)
+    elif x == "<":
+        return(start[0], start[1]-1)
+        j-=1
+    else:
+        return(start[0]-1, start[1])
+
 i,j = (0,0)
 locations = {(0,0): True}
 
 for x in list(open("2015-03-input.txt").readline()):
-    if x == "^":
-        i+=1
-    elif x == ">":
-        j+=1
-    elif x == "<":
-        j-=1
-    else:
-        i-=1
-
+    (i,j) = move((i,j), x)
     locations.update({(i,j): True})
 
 print(len(locations.keys()))
@@ -24,26 +27,11 @@ locations = {(0,0): True}
 
 for x in list(open("2015-03-input.txt").readline()):
     if turn % 2 == 0:
-        a,b = i,j
+        i,j = move((i,j), x)
     else:
-        a,b = p,q
+        p,q = move((p,q), x)
 
-    if x == "^":
-        a+=1
-    elif x == ">":
-        b+=1
-    elif x == "<":
-        b-=1
-    else:
-        a-=1
-
-
-    if turn % 2 == 0:
-        i,j = a,b
-    else:
-        p,q = a,b
-
-    locations.update({(a,b): True})
+    locations.update({(i,j): True, (p,q): True})
     turn += 1
 
 print(len(locations.keys()))
