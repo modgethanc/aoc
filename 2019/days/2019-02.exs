d79621e359656ea65b15a7473b60efa49591ad85
Vincent Zeng <hvincent@modgethanc.com>
2019-12-02 13:06:14 -0500

    d2 complete

--- 2019/days/2019-02.exs
+++ 2019/days/2019-02.exs
@@ -31,6 +31,10 @@ defmodule PartOne do
       calc_unit(List.insert_at(List.delete_at(input, store), store, value), step + 1)
     end
   end
+
+  def transform(input, noun, verb) do
+    List.insert_at(List.delete_at(List.insert_at(List.delete_at(input, 1), 1, noun), 2), 2, verb)
+  end
 end
 
 IO.puts("PART ONE")
@@ -49,25 +53,35 @@ IO.puts(PartOne.calc([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99])
 # puzzle input, read from file and translated to a list of ints
 
 list_input = Enum.map(String.split(String.trim(File.read!("2019-02-input.txt")), ","), &String.to_integer/1)
-
-# apply some transforms
-
-list_input = List.insert_at(List.delete_at(list_input, 1), 1, 12)
-list_input = List.insert_at(List.delete_at(list_input, 2), 2, 2)
-
-# calc
-
-IO.puts(hd(PartOne.calc(list_input)))
+IO.puts(hd(PartOne.calc(PartOne.transform(list_input, 12, 2))))
 
 ## part 2
 
+defmodule PartTwo do
+  def calc(input, target, noun, verb) do
+    brute_force(input, target, noun, verb)
+  end
 
+  def brute_force(input, target, noun, verb) do
+    if hd(PartOne.calc(PartOne.transform(input, noun, verb))) == target do
+      100 * noun + verb
+    else
+      if verb == 99 do
+        brute_force(input, target, noun + 1, 0)
+      else
+        brute_force(input, target, noun, verb + 1)
+      end
+    end
+  end
+end
 
 IO.puts("\nPART TWO")
 
 # test input
 
 IO.write("1.1: ")
-IO.puts(1=1)
+IO.puts(PartTwo.calc(list_input, hd(PartOne.calc(PartOne.transform(list_input, 12, 2))), 12, 2) == 1202)
 
 # puzzle input
+
+IO.puts(PartTwo.calc(list_input, 19690720, 0, 0))
