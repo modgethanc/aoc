7cee7c767dcbad8cfc2e8ac99adf5b20833fe055
Vincent Zeng <hvincent@modgethanc.com>
2019-12-03 14:57:53 -0500

    2019d2p1 can successfully make a list of line coordinates!

--- 2019/days/2019-03.exs
+++ 2019/days/2019-03.exs
@@ -7,17 +7,50 @@
 ## part 1
 
 defmodule PartOne do
+    def crossings(wire1, wire1) do
+        # find conjunction of two lines
+
+    end
+
     def process_wire([head | tail ], loc, coords) do
         input = String.codepoints(head)
         distance = String.to_integer(Enum.join(tl(input)))
-        if hd(input) == "U" do
-            newloc = { elem(loc, 0) + distance, elem(loc, 1) }
-            IO.puts(newloc)
+        newloc =
+        case hd(input) do
+            "U" -> {elem(loc, 0)+distance,elem(loc, 1)}
+            "D" -> {elem(loc, 0)-distance,elem(loc, 1)}
+            "L" -> {elem(loc, 0),elem(loc, 1)-distance}
+            "R" -> {elem(loc, 0),elem(loc, 1)+distance}
         end
-        process_wire(tail, newloc, Map.put(coords, newloc, true))
+        line = draw_line(loc, hd(input), distance, [])
+        #IO.puts(length(line))
+        process_wire(tail, newloc, record_line(line, coords))
     end
 
     def process_wire([], loc, coords) do
+        coords
+    end
+
+    def draw_line(start, direction, distance, points) do
+      #return list of tuples for every point on this line
+      if distance == 0 do
+        points
+      else
+        case direction do
+            "U" -> draw_line({elem(start, 0)+1,elem(start, 1)}, direction, distance-1, [start|points])
+            "D" -> draw_line({elem(start, 0)-1,elem(start, 1)}, direction, distance-1, [start|points])
+            "L" -> draw_line({elem(start, 0),elem(start, 1)-1}, direction, distance-1, [start|points])
+            "R" -> draw_line({elem(start, 0),elem(start, 1)+1}, direction, distance-1, [start|points])
+        end
+      end
+    end
+
+    def record_line([head|tail], coords) do
+      # return a map with all the coord points drawn in
+        record_line(tail, Map.put(coords, head, true))
+    end
+
+    def record_line([], coords) do
         coords
     end
 end
@@ -32,12 +65,17 @@ IO.puts(1=1)
 # puzzle input
 
 
-list_input = String.split(String.trim(File.read!("2019-03-input.txt")))
-wire1 = String.split(hd(list_input), ",")
-wire2 = String.split(hd(tl(list_input)), ",")
+#list_input = String.split(String.trim(File.read!("2019-03-input.txt")))
+#wire1 = String.split(hd(list_input), ",")
+#wire2 = String.split(hd(tl(list_input)), ",")
 
-PartOne.process_wire(wire1, {0,0}, %{})
-|> IO.puts()
+wire1 = String.split("R8,U5,L5,D3", ",")
+wire2 = String.split("U7,R6,D4,L4", ",")
+
+IO.puts(length(Map.keys(PartOne.process_wire(wire1, {0,0}, %{}))))
+PartOne.process_wire(wire2, {0,0}, %{})
+PartOne.process_wire(wire2, {0,0}, %{})
+
 ## part 2
 
 
