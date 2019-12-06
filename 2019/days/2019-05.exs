9e3b6bb6614e30413b734562ee1d4d386aaeb1d2
Vincent Zeng <hvincent@modgethanc.com>
2019-12-05 23:38:23 -0500

    change halt status

--- 2019/days/2019-05.exs
+++ 2019/days/2019-05.exs
@@ -11,18 +11,23 @@ defmodule PartOne do
     calc_unit(input, 0)
   end
 
-  def calc_unit(input, step) do
-    code = Enum.at(input, step*4)
+  def calc_unit(input, pointer) do
+    code = Enum.at(input, pointer)
 
     case code do
-      99 -> input
-      1 -> op1(input, step)
-      2 -> op2(input, step)
-      nil -> input
+      99 -> halt()
+      1 -> op1(input, pointer)
+      2 -> op2(input, pointer)
+      4 -> op4(input, pointer)
     end
   end
 
+  def halt() do
+    "HALT" 
+  end
+
   def op1(input, pointer) do
+    # (a+b)
     op1 = Enum.at(input, pointer+1)
     op2 = Enum.at(input, pointer+2)
     store = Enum.at(input, pointer+3)
@@ -32,12 +37,25 @@ defmodule PartOne do
   end
 
   def op2(input, pointer) do
+    # (a*b)
     op1 = Enum.at(input, pointer+1)
     op2 = Enum.at(input, pointer+2)
     store = Enum.at(input, pointer+3)
     value = Enum.at(input, op1) * Enum.at(input, op2)
 
     calc_unit(List.insert_at(List.delete_at(input, store), store, value), pointer+4)
+  end
+
+  def op3(input, pointer) do
+    # (input)
+  end
+
+  def op4(input, pointer) do
+    # (output)
+    loc = Enum.at(input, pointer+1)
+    Enum.at(input, loc)
+    |> IO.puts()
+    calc_unit(input, pointer+2)
   end
 
   def transform(input, noun, verb) do
@@ -51,7 +69,7 @@ IO.puts("PART ONE")
 
 IO.write("1.1: ")
 IO.puts(PartOne.calc([1,0,0,0,99]) == [2,0,0,0,99])
-
+IO.puts(PartOne.calc([4,0,99]))
 # puzzle input
 
 
